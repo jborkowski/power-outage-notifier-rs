af88ef11434c4a1170ca4d8d3da75143498d8a54
Jonatan Borkowski <mail@j14i.me>
2023-01-11 22:31:32 +0100

    Add simple db

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,48 @@ use chrono::Local;
 use chrono::naive::Days;
 use telegram_bot::*;
 use tokio_stream::StreamExt;
+use tokio_cron_scheduler::{JobScheduler, Job};
+use sqlite::State;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let	 token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
+    let	token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
     let api = Api::new(token);
     let mut stream = api.stream();
+
+
+    let connection = sqlite::open("./database").unwrap();
+
+    let query = "
+      CREATE TABLE IF NOT EXISTS chats (name TEXT, chat_id INTEGER);
+    ";
+
+    connection.execute(query).unwrap();
+
+
+    let sched = JobScheduler::new().await?;
+
+    let check_outages_job = Job::new_async("0 0 * * * *", |_uuid, _l| Box::pin(async move {
+	println!("This will run job at 12:00 am every day");
+        let token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
+        let api = Api::new(&token);
+        let connection = sqlite::open("./database").unwrap();
+
+	if let Some(info) = fetch_info().await.unwrap() {
+	    let query = "SELECT name, chat_id FROM chats";
+	    let mut statement = connection.prepare(query).unwrap();
+
+	    while let Ok(State::Row) = statement.next() {
+		let chat = ChatId::new(statement.read::<i64, _>("chat_id").unwrap());
+		api.spawn(chat.text(&info));
+	    }
+	}
+    })).unwrap();
+
+    sched.add(check_outages_job).await?;
+
+    sched.start().await?;
+
 
     while let Some(update) = stream.next().await {
         let update = update?;
@@ -34,14 +70,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 async fn check_status(api: Api, message: Message) -> Result<(), telegram_bot::Error> {
-    let info = fetch_info().await.unwrap();
-
+    let info = fetch_info().await.unwrap().unwrap_or("There is no planed power outage".to_string());
     api.send(message.text_reply(&info)).await?;
 
     Ok(())
 }
 
-async fn fetch_info() -> Result<String, Box<dyn std::error::Error>> {
+async fn fetch_info() -> Result<Option<String>, Box<dyn std::error::Error>> {
     let resp = reqwest::get("https://pgedystrybucja.pl/planowanewylaczenia/wylaczenia/Legionowo").await?;
     let text = resp.text().await?;
 
@@ -76,10 +111,10 @@ async fn fetch_info() -> Result<String, Box<dyn std::error::Error>> {
 		power_outage_end,
 		streets
 	    );
-            return Ok(message)
+            return Ok(Option::Some(message))
 	}
     }
-    Ok("There is no information".to_owned())
+    Ok(Option::None)
 }
 
 fn try_parse_date(o_row: Option<ElementRef>) -> Option<NaiveDateTime> {
